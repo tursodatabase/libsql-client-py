d0e82168c1deec853b8a893f69cfebe47adc4779
Abdur-RahmaanJ <cryptolabour@gmail.com>
2023-07-12 00:04:27 +0400

    docs: change theme to press

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinxdoc"
+html_theme = "press"
 html_static_path = ["_static"]
 
 intersphinx_mapping = {
